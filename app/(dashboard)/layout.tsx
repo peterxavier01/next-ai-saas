94951ce5621f852877c77d497948ba8ef5534720
Peter Uadiale <fxavy02@gmail.com>
2024-09-24 14:07:53 +0100

    refactor: add logic to display free trial api limit on sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,18 @@
 import Navbar from "@/components/navbar";
 import Sidebar from "@/components/sidebar";
 
-const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+import { getApiLimitCount } from "@/lib/api-limit";
+
+import { isUserSubscribed } from "@/hooks/use-is-subscribed";
+
+const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
+  const apiLimitCount = await getApiLimitCount();
+  const isSubscribed = await isUserSubscribed();
+
   return (
-    <div className="h-full relative">
-      <div className="hidden h-full md:flex md:flex-col md:fixed md:w-72 md:inset-y-0 z-[80] bg-gray-900">
-        <Sidebar />
+    <div className="relative h-full">
+      <div className="hidden h-full bg-gray-900 md:fixed md:inset-y-0 md:flex md:w-72 md:flex-col">
+        <Sidebar apiLimitCount={apiLimitCount} isSubscribed={isSubscribed} />
       </div>
       <main className="md:pl-72">
         <Navbar />
