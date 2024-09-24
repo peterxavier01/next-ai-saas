124aa4f5e4dd494eebeb70104fb1955aaff299e8
Peter Uadiale <fxavy02@gmail.com>
2024-09-24 14:30:29 +0100

    refactor: add free counter component to sidebar ui

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,8 +4,6 @@ import { Montserrat } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
-import { cn } from "@/lib/utils";
 import {
   Code,
   ImageIcon,
@@ -15,6 +13,10 @@ import {
   Settings,
   VideoIcon,
 } from "lucide-react";
+
+import FreeCounter from "@/components/free-counter";
+
+import { cn } from "@/lib/utils";
 
 const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
 
@@ -62,14 +64,19 @@ const routes = [
   },
 ];
 
-const Sidebar = () => {
+interface SidebarProps {
+  apiLimitCount: number;
+  isSubscribed: boolean;
+}
+
+const Sidebar = ({ apiLimitCount = 0, isSubscribed }: SidebarProps) => {
   const pathname = usePathname();
 
   return (
-    <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
-      <div className="px-3 py-2 flex-1">
-        <Link href="/dashboard" className="flex items-center pl-3 mb-14">
-          <div className="relative w-8 h-8 mr-4">
+    <div className="flex h-full flex-col space-y-4 bg-[#111827] py-4 text-white">
+      <div className="flex-1 px-3 py-2">
+        <Link href="/dashboard" className="mb-14 flex items-center pl-3">
+          <div className="relative mr-4 h-8 w-8">
             <Image fill src="/logo.png" alt="logo" />
           </div>
           <h1 className={cn("text-2xl font-bold", montserrat)}>Genius</h1>
@@ -81,20 +88,22 @@ const Sidebar = () => {
               key={route.href}
               href={route.href}
               className={cn(
-                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                "group flex w-full cursor-pointer justify-start rounded-lg p-3 text-sm font-medium transition hover:bg-white/10 hover:text-white",
                 pathname === route.href
-                  ? "text-white bg-white/10"
-                  : "text-zinc-400"
+                  ? "bg-white/10 text-white"
+                  : "text-zinc-400",
               )}
             >
-              <div className="flex items-center flex-1">
-                <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+              <div className="flex flex-1 items-center">
+                <route.icon className={cn("mr-3 h-5 w-5", route.color)} />
                 {route.label}
               </div>
             </Link>
           ))}
         </div>
       </div>
+
+      <FreeCounter apiLimitCount={apiLimitCount} isSubscribed={isSubscribed} />
     </div>
   );
 };
