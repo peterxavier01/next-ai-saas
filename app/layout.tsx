23d00af252a3441bb554b1fdf15fb05ef1d404e7
Peter Uadiale <fxavy02@gmail.com>
2024-09-24 14:07:01 +0100

    refactor: add modal provider and toaster to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
+import ModalProvider from "@/components/modal-provider";
+import { Toaster } from "@/components/ui/sonner";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,7 +21,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+          <Toaster />
+        </body>
       </html>
     </ClerkProvider>
   );
