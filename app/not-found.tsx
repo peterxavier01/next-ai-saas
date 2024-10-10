62078df99c29c14aee35543c20006d2ac9aea391
Peter Uadiale <fxavy02@gmail.com>
2024-10-10 13:25:06 +0000

    style: modify styles

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,8 +6,9 @@ import { Button } from '@/components/ui/button'
 
 export default function NotFound() {
     const router = useRouter()
+
     return (
-        <main className='w-full min-h-dvh flex flex-col items-center justify-center bg-black text-white gap-4'>
+        <main className='w-full min-h-dvh flex flex-col items-center justify-center gap-4'>
             <h2 className='text-xl md:text-3xl lg:text-5xl font-bold'>Not Found</h2>
             <p className='md:text-lg font-medium'>Could not find the requested page.</p>
             <Button onClick={() => router.back()}>Go Back</Button>
